bbd51db66707755f37b2a4ae2a0c223eaa1eafd5
Brian Lowry <brianlowry@outlook.com>
2018-11-05 19:00:15 -0700

    redux props

--- src/actions/index.js
+++ src/actions/index.js
@@ -32,10 +32,10 @@ export const guessWord = guessedWord => {
 
 export const getSecretWord = () => {
   return dispatch => {
-    return axios.get('http://localhost:3030').then(response => {
+    return axios.get('http://www.randomtext.me/api/').then(response => {
       dispatch({
         type: actionTypes.SET_SECRET_WORD,
-        payload: response.data
+        payload: 'party'
       });
     });
   };
--- src/app.js
+++ src/app.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
+import { connect } from 'react-redux';
 import './app.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
-        />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default hot(module)(App);
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSecretWord }
+)(App);
